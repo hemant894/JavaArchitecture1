c00ea8743818f24d39d4d8827711068b0a8e2f89
Hemant <hemant894@gmail.com>
2022-12-20 18:41:44 +0000

    JavaArchitecture

---
+++ JavaArchitecture.java
@@ -0,0 +1,15 @@
+package JavaArchitecture;
+
+public class JavaArchitecture {
+    public static void main(String[] args) { System.out.println(" JVM (Java Virtual Machine) is an abstract machine. it is a specification that provides runtime environment in which java bytecode can be excuted.");
+        System.out.println(" JRE (Java Runtime Environment) is a runtime environment which implements JVM and provides all class libraries and other files that JVM uses at runtime.");
+        System.out.println(" JDK (Java Development kit ) is the tool necessary to compile, document and package java programs. The JDK completely includes JRE.");
+        System.out.println(" $$$$$ JAVA VARIABLE $$$$$  ");
+        System.out.println(" Variables are nothing but reserved memory locations to store data values by using this values we are achieving the project requirements. This can be any kind of information ranging from texts , numbers , sentences etc.");
+        System.out.println(" This means that when you create a variable you reserve some space in the memory. A variable thus has a data type.");
+        System.out.println(" In simple words we can say variables are containers that holds the data.");
+        System.out.println(" Variable Declaration and initialisation ");
+
+
+    }
+}
